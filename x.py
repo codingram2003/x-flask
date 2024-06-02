3fc23e0eb5b9fad2e08656933b5dd5d3ec3e4aa6
Shriram <shriramrayakar@gmail.com>
2024-06-02 19:55:46 +0530

    initial commit

--- x.py
+++ x.py
@@ -20,13 +20,13 @@ url = "https://x.com/i/flow/login"
 def path():  
     global chrome
     # Set up the Selenium WebDriver
-    ''' options = webdriver.ChromeOptions()
+    options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument('--headless')
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-extensions')
-    options.add_argument('--disable-gpu') '''
+    options.add_argument('--disable-gpu') 
 
 
 
